3c4cac05f252bb5440a603fe776fd6c30b73ba98
cilercx <41168697+cilercx@users.noreply.github.com>
2018-07-30 23:08:45 +0300

    Javascript ve form çalışmaları yapıldı.

---
+++ JS/Form/main.js
@@ -0,0 +1,16 @@
+// var myusername = $("#username").val();
+
+// function btnGonderMetod() {   
+// $.ajax({
+//   type: "GET",
+//   url: "index.html",
+//   data: myusername,
+//   cache: false,
+//   success: function(data){
+//      $("#resultarea").text(data);
+//   }
+// })
+// }
+
+
+
